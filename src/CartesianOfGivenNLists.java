19cf34427a2dd2fbe6de2c140b555c44ecbfec26
sumit-hanumante <sumithanumanteblog@gmail.com>
2018-09-14 09:23:20 +0530

    initial commit

---
+++ src/CartesianOfGivenNLists.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ Example 1 (2 lists as input) :
+    Input : [ [a,b] , [p,q] ]
+    Output : [ [a,p], [a,q], [b,p], [b,q] ]
+
+
+ Example 2 (3 lists as input):
+        Input : [ [a,b] , [p,q] , [l,m] ]
+        Output : [ [a,p,l], [a,p,m], [a,q,l], [a,q, m] , [b,p,l] , [b,p,m] , [b,q,l] , [b,q,m] ]
+ *
+ */
+
+public class CartesianOfGivenNLists {
+
+    private List<List<String>> cartesian(List<List<String>> inputLists) {
+
+        List<List<String>> result = new ArrayList<>();
+
+        if (inputLists.size()==0){
+            result.add(new ArrayList<>());
+            return result;
+        }
+
+
+        List<String> firstList = inputLists.get(0);
+        List<List<String>> remainingList = cartesian(inputLists.subList(1,inputLists.size()));
+
+        for (String firstWord: firstList){
+            for (List<String> remainingWords: remainingList){
+                List<String> temp = new ArrayList<>();
+                temp.add(firstWord);
+                temp.addAll(remainingWords);
+                result.add(temp);
+            }
+        }
+
+        return result;
+    }
+}
